16ec0b696a5be04eb4a2bf206764f1f88395293b
Abhishek Goyal <abhishek252167@gmail.com>
2020-08-07 22:24:15 +0530

    fixed and renamed

--- git_push_all_files.py
+++ git_push_all_files.py
@@ -1,3 +1,5 @@
+import os
+
 def main():
     for filename in os.listdir():
         command = 'git add ' + filename
@@ -7,6 +9,8 @@ def main():
         command = f'git commit -m "{commit_msg}"'
         print(command)
         os.system(command)
+    print('git push')
+    os.system('git push')
 
 if __name__ == '__main__':
     main() 
