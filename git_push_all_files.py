d8680049d0fcef2d8ec09bb8dd0fe5b8ce668469
Abhishek Goyal <abhishek252167@gmail.com>
2020-08-26 00:17:06 +0530

    updated

--- git_push_all_files.py
+++ git_push_all_files.py
@@ -2,15 +2,15 @@ import os
 
 def main():
     for filename in os.listdir():
-        command = 'git add ' + filename
+        command = f'git add {filename}'
         print(command)
         os.system(command)
         commit_msg = f'Added {filename}'
         command = f'git commit -m "{commit_msg}"'
         print(command)
         os.system(command)
-    print('git push')
-    os.system('git push')
+        print('git push')
+        os.system('git push')
 
 if __name__ == '__main__':
     main() 
--- rename_files.py
+++ rename_files.py
@@ -2,7 +2,7 @@ import os
 
 def main():
     for filename in os.listdir():
-        if filename != 'rename_multiple_files.py':
+        if filename != 'rename_files.py':
             new_filename = 
             print(f'{filename} -> {new_filename}')
             # os.rename(filename, new_filename)
